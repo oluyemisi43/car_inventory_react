8e9ff449939258cbd2799d08ad26dbe7d47804e0
oluyemisi olusola <77764812+oluyemisi43@users.noreply.github.com>
2023-11-08 15:44:36 -0500

    display datatable

--- car-inventory/src/components/Dashboard/Dashboard.tsx
+++ car-inventory/src/components/Dashboard/Dashboard.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { DataTable } from '../../components';
 
 export const Dashboard = (props:any) => {
     return (
         <div>
             <h1>{props.title}</h1>
+            <DataTable></DataTable>    
         </div>
     )
 }
